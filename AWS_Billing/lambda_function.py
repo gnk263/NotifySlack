ea810cc6d47aa1f906d1f0260bac81d69e4ea2de
gnk263 <gnk263@users.noreply.github.com>
2018-07-01 23:49:23 +0900

    slackに関する環境変数を取得する

--- AWS_Billing/lambda_function.py
+++ AWS_Billing/lambda_function.py
@@ -1,3 +1,8 @@
+import os
+
+SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
+SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
+
 def lambda_handler(event, context):
     # TODO implement
     return 'Hello from Lambda'
