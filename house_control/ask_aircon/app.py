2c7b8bcc3abfab830957274f83bd566225979868
gnk263 <gnk263@users.noreply.github.com>
2018-08-28 23:37:35 +0900

    Lambdaコードのサンプルを削除してHelloWorld出力に変更 #5
    下記コマンドでhelloworldの出力を確認
    
    echo {""} | sam local invoke HelloWorldFunction

--- house_control/ask_aircon/app.py
+++ house_control/ask_aircon/app.py
@@ -4,22 +4,5 @@ import requests
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
 
-    Arguments:
-        event LambdaEvent -- Lambda Event received from Invoke API
-        context LambdaContext -- Lambda Context runtime methods and attributes
-
-    Returns:
-        dict -- {'statusCode': int, 'body': dict}
-    """
-
-    ip = requests.get('http://checkip.amazonaws.com/')
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            'message': 'hello world',
-            'location': ip.text.replace('\n', ''),
-        })
-    }
+    print("hello world")
