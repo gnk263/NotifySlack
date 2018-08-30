f57fec5d78ad717e8d3e7ac70af51b544d43c6ea
gnk263 <gnk263@users.noreply.github.com>
2018-08-30 22:10:43 +0900

    slackへの通知処理を実装 #5

--- house_control/ask_aircon/app.py
+++ house_control/ask_aircon/app.py
@@ -3,9 +3,25 @@ import json
 
 import requests
 
+SLACK_WEBHOOK_URL = os.environ["SLACK_WEBHOOK_URL"]
+SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
 
 def lambda_handler(event, context):
 
     print("hello world")
-    print(os.environ["SLACK_WEBHOOK_URL"])
-    print(os.environ["SLACK_CHANNEL"])
+
+    post_slack("This is test message.")
+
+def post_slack(message):
+    payload = {
+        "text": message,
+        "channel": SLACK_CHANNEL
+    }
+
+    # http://requests-docs-ja.readthedocs.io/en/latest/user/quickstart/
+    try:
+        response = requests.post(SLACK_WEBHOOK_URL, data=json.dumps(payload))
+    except requests.exceptions.RequestException as e:
+        print(e)
+    else:
+        print(response.status_code)
