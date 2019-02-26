dcc2299f3374ff20650175effb0e42dc3dcab886
gnk263 <gnk263@users.noreply.github.com>
2019-02-26 10:16:52 +0900

    デプロイ時のStack名を修正 #8

--- TrainDelay/03_deploy.sh
+++ TrainDelay/03_deploy.sh
@@ -4,6 +4,6 @@ WEBHOOK_URL="https://hoge"
 
 sam deploy \
     --template-file packaged.yaml \
-    --stack-name NotifyBillingToSlack \
+    --stack-name NotifyTrainDelayToSlack \
     --capabilities CAPABILITY_IAM \
     --parameter-overrides SlackWebhookUrl=$WEBHOOK_URL
